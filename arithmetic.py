bb2261c0a6906eae993d61351e0c196b9485e0c5
NandanaHasheed <nandanahasheed2028@cs.sjcetpalai.ac.in>
2024-10-05 15:44:34 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,9 @@
+a=10
+b=5
+print("Sum =:",a+b)
+print("Division=:",a/b)
+print("Is a greater than b?:",a>b)
+print("Are a and b equal ?:",a==b)
+print(" Logical AND:",a>b and a<b)
+print("Logical OR:",a>b or a<b)
+
