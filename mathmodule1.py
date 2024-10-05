15db153a056a2460dfa04306f886dc1d6df725da
NandanaHasheed <nandanahasheed2028@cs.sjcetpalai.ac.in>
2024-10-05 15:56:22 +0530

    Add files via upload

---
+++ mathmodule1.py
@@ -0,0 +1,5 @@
+import math
+number=int(input("Enter a number: "))
+print(" Square root of ",number,":",math.sqrt(number))
+print("Factorial of ",number,":",math.factorial(number))
+print("5 raised to power 2",number,":",math.pow(number,2))
