001a12ca4467caa0e15ba70d8452ec3936b183d8
NandanaHasheed <nandanahasheed2028@cs.sjcetpalai.ac.in>
2024-10-08 15:39:36 +0530

    Update age.py

--- age.py
+++ age.py
@@ -1,3 +1,9 @@
+'''
+Author: Nandana Hasheed
+Date: 08-10-2024
+python program to print name and age 
+version 1.0
+'''
 name=input("Enter your name :")
 age=int(input("Enter age :"))
 print("Hello ",name,"! you are ",age,"year old")
